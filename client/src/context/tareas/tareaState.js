c598f7c6ce6b58b310de34e8525661562e4fa555
raidentek <33987212+raidentek@users.noreply.github.com>
2021-01-14 10:40:16 -0500

    reducer

--- client/src/context/tareas/tareaState.js
+++ client/src/context/tareas/tareaState.js
@@ -18,6 +18,7 @@ const TareaState = (props) => {
       { id: 9, nombre: "sitio web", estado: false, proyectoId: 3 },
       { id: 10, nombre: "Eligir plataforma", estado: true, proyectoId: 1 },
       { id: 11, nombre: "Elegir colores", estado: false, proyectoId: 2 },
+      { id: 12, nombre: "Elegir colores", estado: true, proyectoId: 3 },
     ],
     tareasProyecto: null,
   };
